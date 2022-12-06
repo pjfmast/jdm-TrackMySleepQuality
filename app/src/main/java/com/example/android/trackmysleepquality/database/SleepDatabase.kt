66e495a23f09a29d279427d0ef5a210d0fc78747
Paul de Mast <pjfmast@gmail.com>
2022-12-06 10:39:21 +0100

    Solution for TrackMySleepQuality with updated dependencies and using new guidelines for Room and singleton

---
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * A database that stores SleepNight information.
+ * And a global method to get access to the database.
+ *
+ * This pattern is pretty much the same for any database,
+ * so you can reuse it.
+ */
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    /**
+     * Connects the database to the DAO.
+     */
+    abstract fun sleepDatabaseDao(): SleepDatabaseDao
+
+    /**
+     * Define a companion object, this allows us to add functions on the SleepDatabase class.
+     *
+     * For example, clients can call `SleepDatabase.getInstance(context)` to instantiate
+     * a new SleepDatabase.
+     */
+    companion object {
+        /**
+         * INSTANCE will keep a reference to any database returned via getInstance.
+         *
+         * This will help us avoid repeatedly initializing the database, which is expensive.
+         *
+         *  The value of a volatile variable will never be cached, and all writes and
+         *  reads will be done to and from the main memory. It means that changes made by one
+         *  thread to shared data are visible to other threads.
+         */
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        /**
+         * Helper function to get the database.
+         *
+         * If a database has already been retrieved, the previous database will be returned.
+         * Otherwise, create a new database.
+         *
+         * This function is threadsafe, and callers should cache the result for multiple database
+         * calls to avoid overhead.
+         *
+         * @param context The application context Singleton, used to get access to the filesystem.
+         */
+        fun getInstance(context: Context): SleepDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SleepDatabase::class.java,
+                    "sleep_history_database"
+                )
+                    // Wipes and rebuilds instead of migrating if no Migration object.
+                    // Migration is not part of this codelab.
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
